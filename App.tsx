c9c3fe4d430637d3fa9f28ce1cca8396c1c509f9
Penguin5681 <pranavsinha922@gmail.com>
2024-11-29 21:46:41 +0530

    HELL YEAH: Navigation Bug Fixed

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import './gesture-handler';
 import RootNavigation from "./src/Navigation/RootNavigation/RootNavigation.tsx";
 import {NavigationContainer} from "@react-navigation/native";
 
--- src/Navigation/RootNavigation/RootNavigation.tsx
+++ src/Navigation/RootNavigation/RootNavigation.tsx
@@ -1,17 +1,18 @@
 import {RootRoutes} from './RootRoutes.ts';
 import TestScreen1 from '../../Screens/TestScreen1/TestScreen1.tsx';
 import TestScreen2 from '../../Screens/TestScreen2/TestScreen2.tsx';
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {createStackNavigator} from "@react-navigation/stack";
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 const RootNavigation = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name={RootRoutes.TestScreen1} component={TestScreen1} />
-      <Stack.Screen name={RootRoutes.TestScreen2} component={TestScreen2} />
-    </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator
+			initialRouteName={RootRoutes.TestScreen1}>
+			<Stack.Screen name={RootRoutes.TestScreen1} component={TestScreen1} />
+			<Stack.Screen name={RootRoutes.TestScreen2} component={TestScreen2} />
+		</Stack.Navigator>
+	);
 };
 
 export default RootNavigation;
